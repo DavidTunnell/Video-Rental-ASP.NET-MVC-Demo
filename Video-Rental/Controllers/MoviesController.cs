2363d7ef162921a3d1bb704862e9a4cb951aa422
David Tunnell <davidtunnell9@gmail.com>
2021-05-29 16:44:24 -0500

    first commit

---
+++ Video-Rental/Controllers/MoviesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Video_Rental.Models;
+
+namespace Video_Rental.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            
+            var movie = new Movie() { Name= "Shrek!"};
+            return View(movie);
+        }
+    }
+}
