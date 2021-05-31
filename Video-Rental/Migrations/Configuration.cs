d60830e9282ba381a731a80001d62e453794566b
David Tunnell <davidtunnell9@gmail.com>
2021-05-31 13:15:59 -0500

    Added entity framework including connection to custom model. Initalized database (mdf).

---
+++ Video-Rental/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace Video_Rental.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Video_Rental.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Video_Rental.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
--- Video-Rental/Models/IdentityModels.cs
+++ Video-Rental/Models/IdentityModels.cs
@@ -20,6 +20,8 @@ namespace Video_Rental.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+
+        public DbSet<Customer> Customers { get; set; }
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
